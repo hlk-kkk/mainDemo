3ec08e40ce15a2a33639257ee5b590b3a979d1b7
hlk <480587479@qq.com>
2019-08-06 16:52:24 +0800

    config服务端与客户端配置

---
+++ config-client/src/main/java/com/hlk/demo/HomeController.java
@@ -0,0 +1,17 @@
+package com.hlk.demo;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RefreshScope
+@RestController
+public class HomeController {
+    @Value("${config.hello}")
+    private String hello;
+
+    @RequestMapping("/hello")
+    public String from() {
+        return this.hello;
+    }
+}
